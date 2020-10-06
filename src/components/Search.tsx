9a65ad8e15eeb45cf21de7d23bf4faeb5475d13e
gbengaPS <ioyetade@gmail.com>
2020-10-06 08:38:44 +0100

    design search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
-import { getCityFromAPI } from '../helpers';
+import { getCityFromAPI, getIconURL } from '../helpers';
 
 
 const Search = () => {
   const [searchInput, setSearchInput] = useState('');
   const [searchResult, setSearchResult] = useState<any>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchError, setFetchError] = useState('')
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
-    const response = await getCityFromAPI({ q: searchInput })
 
+    setFetchError('');
+    setIsLoading(true);
+    setSearchResult(null);
+
+    const response = await getCityFromAPI({ q: searchInput })
+    setIsLoading(false);
+  
     if (response) {
       setSearchResult(response.data)
+    } else {
+      setFetchError('Your search returned no results.');
     }
    
   }
@@ -20,7 +30,8 @@ const Search = () => {
     const userInput = event.target.value;
 
     if (!userInput) {
-      setSearchResult(null)
+      setSearchResult(null);
+      setFetchError('');
     }
   
     setSearchInput(userInput);
@@ -30,20 +41,36 @@ const Search = () => {
     if (!searchInput || !searchResult) {
         return null;
     }
-  
+    
     if (searchResult) {
+      const weatherDescription = searchResult.weather[0].description
       return (
-        <div>
-          <p>Temp: {Math.floor(searchResult.main.temp)} &#8451;</p>
-          <p>Min Temp: {Math.floor(searchResult.main.temp_min)} &#8451;</p>
-          <p>Max Temp: {Math.floor(searchResult.main.temp_max)} &#8451;</p>
+        <div className="search-result">
+          <div className="aligned-flex">
+          <div className="search-result-details">
+            <img src={getIconURL(searchResult.weather[0].icon)} alt={weatherDescription} />
+            <p>{weatherDescription}</p>
+          </div>
+          <div className="search-result-details">
+            <p className="main-temp">{Math.floor(searchResult.main.temp)}&#730;</p>
+            <p>Feels like {Math.floor(searchResult.main.feels_like)}&#730;</p>
+          </div>
+        </div >
+          <p className="aligned-flex">
+            <span>High {Math.floor(searchResult.main.temp_max)}&#730;</span>
+            |
+            <span>Low {Math.floor(searchResult.main.temp_min)}&#730;</span>
+            |
+            <span>Humidity {searchResult.main.humidity}% </span>
+          </p>
         </div>
       )
     }
   
   }
   return (
-    <div>
+    <>
+    <div className="search-wrapper">
       <form onSubmit={handleSubmit}>
         <input
           type="search"
@@ -53,10 +80,16 @@ const Search = () => {
           onChange={handleChange}
           aria-label="Search city weather information"
         />
-        <button>Search</button>
+        <button><i className="fa fa-search" aria-hidden="true"></i></button>
       </form>
+      
+    </div>
+    <div className="search-result-wrapper">
+      { isLoading && <img src="/loader.gif"  alt="loading indicator"/>}
+      { fetchError && <p className="error">{fetchError}</p>}
       {renderSearchResult()}
     </div>
+    </>
   )
 }
 
